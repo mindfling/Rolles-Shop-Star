e6f78fe38ffa478625736f0a0ba765e2df047cb8
Nepochtov Dmitry <dimendroider@gmail.com>
2025-01-11 04:25:18 +0300

    Update products.js

--- rolli-shop-start-project/data/products.js
+++ rolli-shop-start-project/data/products.js
@@ -3,53 +3,40 @@ export const products = [
     id: '01',
     image: 'img/roll/philadelphia.jpg',
     title: 'Филадельфия хит ролл',
-    number: '6', // 6 шт.
-    description: '6 штук Ролл Филадельфия',
-    weight: '180', // '180г.'
-    price: '300', // '300 ₽'
+    number: 6, // 6 шт.
+    description: 'Ролл Филадельфия хит',
+    weight: 180, // '180г.'
+    price: 300, // '300 ₽'
     currency: '₽',
   },
   {
     id: '02',
     image: 'img/roll/california-tempura.jpg',
     title: 'Калифорния темпура',
-    number: '8 шт.',
-    description: '8 штук Калифорния темпура',
-    weight: '205г.',
-    price: '250',
+    number: 8,
+    description: 'Калифорния темпура',
+    weight: 205,
+    price: 250,
     currency: '₽',
   },
   {
     id: '03',
     image: 'img/roll/zapech-california.jpg',
     title: 'Запеченый ролл «Калифорния»',
-    number: '6 шт.',
-    description: '6 штук Запеченных ролл «Калифорния»',
-    weight: '182г.',
-    price: '230',
+    number: 6,
+    description: 'Запеченных ролл «Калифорния»',
+    weight: 182,
+    price: 230,
     currency: '₽',
   },
   {
     id: '04',
     image: 'img/roll/philadelphia.jpg',
     title: 'Филадельфия',
-    number: '6 шт.',
-    description: '6 штук Ролл "Филадельфия"',
-    weight: '230г.',
-    price: '320',
+    number: 3,
+    description: 'Ролл «Филадельфия»',
+    weight: 230,
+    price: 320,
     currency: '₽',
   },
 ]
-
-/*
-{
-  id: '0',
-  image: 'img/roll/.jpg',
-  title: 'Ролл',
-  number: '6 шт.',
-  description: '6 штук Ролл',
-  weight: 'г.',
-  price: '',
-  currency: '₽',
-},
-*/
